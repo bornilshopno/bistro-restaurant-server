995d60042603bdd63287f368b61d1d13560fde90
Ashraf Manna <optimisticashraf@gmail.com>
2025-01-06 18:55:34 +0600

    cart delete option gernerated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express()
 const port= process.env.PORT || 5000
 
@@ -53,6 +53,12 @@ async function run() {
     app.get("/reviews" ,async(req,res)=>{
         const result = await reviewsCollection.find().toArray();
         res.send(result)
+    })
+    app.delete("/carts/:id", async(req,res)=>{
+      const id=req.params.id;
+      const query={_id: new ObjectId(id)};
+      const result=await cartsCollection.deleteOne(query);
+      res.send(result);
     })
 
 // Send a ping to confirm a successful connection
